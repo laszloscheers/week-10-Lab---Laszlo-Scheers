63221d80558fff32511e79517bb6e920f33a8647
Laszlo-VSC <laszloluque@gmail.com>
2021-12-30 15:32:30 +0000

    Question 6

--- Sum/script.js
+++ Sum/script.js
@@ -1,14 +1,10 @@
-function play(){
-    var results = [];
-    var sum;
-    document.getElementById("results").innerHTML=`<sapn id="bold">Result:</sapn><br><br>`;
-    for(i=0;i<5;i++){
-        results[i]=Math.floor((Math.random()*20)+1);
-        console.log("Results"+(i+1)+"="+results[i]);
-    }
-    sum=0;
-    for(i=0;i<5;i++){
-        sum+=results[i];
-    }
-    document.getElementById("results").innerHTML+="The array is "+results.toString(",")+".<br><br>The sum is "+sum+".";
+var listNumber=5;
+function change(listNumber){
+    document.getElementById(listNumber).innerHTML=prompt("Please, enter the new item");
+}
+function add(){
+    var groceryItems = ["Ruibarb","Eggs","Bread Loaf","Flour","Coffee"];
+    var item=groceryItems[Math.floor(Math.random()*5)];
+    listNumber++;
+    document.getElementById("list").innerHTML+=`<li><div class="list"><div id="list`+listNumber+`">`+item+`</div><div class="rigth"><button class="button-2" onclick="change('list`+listNumber+`')">Change item</button></div></div></li>`;
 }
